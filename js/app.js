12df43714321b7a8f184ea45c953bbd68d549720
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-04-04 13:50:02 +0530

    Add deleting effect

--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@ var texts = [
 var startingTime = null;
 var i = 0;
 var j = 0;
+var k = 0;
 var elmStage = document.getElementById('stage');
+var reverse = false;
 
 function animateText(stamp){
 
@@ -17,15 +19,27 @@ function animateText(stamp){
 
     var interval = stamp - startingTime;
 
-    if (interval >= 150){
+    if (interval >= 70 && reverse){
+        var text = texts[j];
+        elmStage.innerText = text.substring(0, k--);
+
+        if (k === 0){
+            j++;
+            reverse = false;
+
+            if (j >= texts.length) j = 0;
+        }
+    }
+
+    if (interval >= 150 && !reverse){
         startingTime = stamp;
 
         var text = texts[j];
         elmStage.innerText = text.substring(0, i++);
         if (i > (text.length + 10)) {
+            reverse = true;
             i = 0;
-            j++;
-            if (j === texts.length) j = 0;
+            k = text.length;
         }
     }
 
